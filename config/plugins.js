334ca7a8e3a5c846ac18761079f3a96eda361121
pavloseldemirov-max <pavlo.seldemirov@gr8.tech>
2026-02-16 16:15:58 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+export default () => ({
+  'preview-button': {
+    config: {
+      contentTypes: [
+        {
+          uid: 'api::cashierinfopage.cashierinfopage',
+          published: {
+            url: 'https://fruitful-desire-5699eb9ec8.strapiapp.com',
+          },
+        },
+      ],
+    },
+  },
+});
